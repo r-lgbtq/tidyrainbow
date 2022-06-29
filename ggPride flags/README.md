5f5ec0b9774785f447880d708c0be7bcbf82e4d1
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-06-29 15:40:24 -0600

    init flag palette setup

---
+++ ggPride flags/README.md
@@ -0,0 +1,4 @@
+
+# ggPride flags
+
+This is a idea for making an official Pride flag color palette for ggplot2. 
---
+++ ggPride flags/pride_flags.R
@@ -0,0 +1,51 @@
+
+library(tidyverse)
+
+#  red => purple
+pride_flag = c('#FF0909','#FF9B3C','#FFEE39','009A3A','#002F97','#5C1079')
+
+# Gay blues
+gay_flag = c('#74BEFC','#98CFFD','#C6E5FE','#77AFE1','#0083CA','#002F70')
+
+
+# lesbian maroon /reds
+lesbian_flag = c('#95014D','#AA3C7D','#DF9AC3','#DF9AC3','#BC3643','#74BEFC')
+
+# bisexual 3 colors
+bisexual_flag = c('#D0035D','#5C3884','#001C95')
+
+
+# pansexual flag 3 colors
+pansexual_flag = c('#FF0579','#FFD333','#009FFB')
+
+# asexual flag
+asexual_flag = c('#000000','#6C6C6C','#FFFFFF','#520051')
+
+# intersex flag 2 colors, best for diverging purposes
+intersex_flag = c('#FFDA34','#7B01A8')
+
+# transgender flag
+transgender_flag = c('#00BFFB','#FFFFFF','#FFBFFD')
+
+# genderfluid flag
+genderfluid_flag = c('#FE6093','#AD00CF','#000000','#0620B1')
+
+# agender flag
+agender_flag = c('#AFB9BD','#ACF572','#000000')
+
+# demiboy
+demiboy_flag = c('#84D4E7','#B7B6B9','#6C6C6E')
+
+# demigirl
+demigirl_flag = c('#FF9BBB','#B7B6B9','#6C6C6E')
+
+# non-binary flag
+nonbinary_flag = c('#FFF546','#FFFFFF','#883BC1','#1C1C1C')
+
+# genderqueer flag
+genderqueer_flag = c('#CE7EDE','#FFFFFF','#288225')
+
+
+
+
+
